71ec51fb45844e08937b4bc2dc1107c2bf7df6a2
sava28 <113110514+sava2808@users.noreply.github.com>
2024-05-30 19:51:18 +0700

    2 todos done
    TASKDONE::thing creation dropdown(dropup?);;change object editing index
    object reference on clone my deeply hated, structuredClone() my savior

--- scripts/editor.js
+++ scripts/editor.js
@@ -2,32 +2,56 @@
 // editor related stuff
 //
 editobj = { // properties of object being currently edited
-  objx:0,
-  objy:0,
-  html:"",
+	objx:0,
+	objy:0,
+	html:"",
 }
 editor = { //current things being edited
-  scene:0,
-  obj:0,
-  keyframe:0,
+	scene:0,
+	obj:0,
+	keyframe:0,
 }
-
+templateobj = { //default flacss square
+	html:"<img src='images/flacss_logo.png'>",
+	objx:0,objy:0,
+	objstart:1,objend:3,
+	keyframes:[{keystart:1,keyend:3,keytype:"transform",transform:"rotate",keyvalue:360,easing:"linear"}]
+}
 function updateEditing(elem,type){ //update editing values of input
-  switch (type) {
-	  case "object":
-	  eval("editobj." + elem.id + " = elem.value")
-	  break
-  }
+	switch (type) {
+		case "object":
+		eval("editobj." + elem.id + " = elem.value")
+		break
+	}
 }
 function saveChanges() { //save editing changes
-  for(key in editobj) {
-	movie.scenes[editor.scene].objects[editor.obj][key] = editobj[key]
-  }
+	for(key in editobj) {
+		movie.scenes[editor.scene].objects[editor.obj][key] = editobj[key]
+	}
 }
 function updateInfoMenu(){ //updates the infomenu (object&keyframe info) based on what's selected
-  object = movie.scenes[editor.scene].objects[editor.obj]
-  for(key in object){
-	editobj[key] = object[key]
-	document.getElementById(key).value = object[key]
-  }
+	editor.obj = Math.min(Math.max(editor.obj,0),movie.scenes[editor.scene].objects.length - 1)
+	document.getElementById("objindex").value = editor.obj
+	object = movie.scenes[editor.scene].objects[editor.obj]
+	for(key in object){
+		editobj[key] = object[key]
+		document.getElementById(key).value = object[key]
+	}
+}
+function createThing(thing) { //create a new keyframe/object/scene, handle dropdown
+	switch (thing) {
+		case "dropdown":
+		drop = document.getElementsByClassName("create-details")[0]
+		if (drop.getAttribute("closed") == undefined) {
+			drop.setAttribute("closed","")
+		} else {
+			drop.removeAttribute("closed")
+		}
+		break
+		case "object":
+		movie.scenes[editor.scene].objects.push(structuredClone(templateobj))
+		editor.obj = movie.scenes[editor.scene].objects.length - 1
+		updateInfoMenu()
+		break
+	}
 }
--- scripts/parse.js
+++ scripts/parse.js
@@ -2,19 +2,19 @@
 // stuff related to creating the preview from movie data
 //
 movietemplate = { // the (default) 'movie' to convert and play back 
-  title: "FlaCSS Movie",
-  description: "Default Template",
-  scenes: [ //scene list
-    { //scene
-      objects: [ //scene object list
-	    { //object
+	title: "FlaCSS Movie",
+	description: "Default Template",
+	scenes: [ //scene list
+		{ //scene
+			objects: [ //scene object list
+			{ //object
 			html:"<span style='font-size:24px'>Welcome to FlaCSS</span>", //html inside object 
 			objx:100, //object starting x position
 			objy:100, //object starting y position (0,0 = top left)
 			objstart:1, //object spawning time (seconds)
 			objend:6, //object despawning time (seconds)
 			keyframes:[ //keyframe list
-			  { //keyframe
+				{ //keyframe
 				keytype:"transform",
 				keystart:2, //keyframe start time (seconds)
 				keyend:4, //keyframe end time (seconds)
@@ -22,62 +22,61 @@ movietemplate = { // the (default) 'movie' to convert and play back
 				transform:"translate",
 				transformvalue:[200,200], //translation is relative 
 				easing:"ease-in", //css animation easing functions
-			  }, //keyframe end
-			  /*{ 
+				}, //keyframe end
+				/*{ 
 				keytype:"fade",
 				keystart:2,
 				keyend:4,
 				fadedir:1, //1 = fade in; 0 = fade out
 				easing:"ease-in",
-			  },*/ //this doesn't work, the cohost go animations (https://cohost.org/blep/post/5112453--)
-				   //cant have their transform changed by css :( 
-				   //if someone can fix this lemme know 
+				},*/ //this doesn't work, the cohost go animations (https://cohost.org/blep/post/5112453--)
+					 //cant have their transform changed by css :( 
+					 //if someone can fix this lemme know 
 			], //keyframe list end
 			
 		}, //object end
-      ], //scene object list end
-      
-    }, //scene end
-  ], //scene list end
+			], //scene object list end
+			
+		}, //scene end
+	], //scene list end
 }
 movie = {
-  title: "TestMovie",
-  description: "Test",
-  scenes:[
+	title: "TestMovie",
+	description: "Test",
+	scenes:[
 	{
-		objects:[{
-		html:"<img src='images/flacss_logo.png'>",objx:0,objy:0,objstart:1,objend:3,
-		keyframes:[{},{}]
-		}]
+		objects:[structuredClone(templateobj)]
 	}
-  ]
+	]
 }
-// function keySort(a,b){
-  // tr_priority = {translate:1,translate3d:1,rotate:2,rotate3d:2,scale:3}
-  // if (a.keytype == "transform") {
-	  // an = tr_priority[a.transform]
-  // }
-  // if (b.keytype == "transform") {
-	  // bn = tr_priority[b.transform]
-  // }
+// function sortKeys(a,b){
+	// tr_priority = {translate:1,translate3d:1,rotate:2,rotate3d:2,scale:3} //priority list, high = deeper animation
+	// if (a.keytype == "transform") {
+		// an = tr_priority[a.transform]
+	// }
+	// if (b.keytype == "transform") {
+		// bn = tr_priority[b.transform]
+	// }
+	//return (bn - an)
 // }
-function createPreview() { //create projector preview 
-  proj = document.getElementsByClassName("projector")[0]
-  proj.innerHTML = "" //reset
-  for (scene of movie.scenes) {
-	  for (object of scene.objects) {
-		  //keylayers = []
-		  //keysort = object.keyframes
-		  //keysort.sort()
-		  obj = document.createElement("div")
-		  obj.innerHTML = object.html
-		  obj.style = `position:absolute;left:${object.objx}px;top:${object.objy}px;transform:rotatex(90deg);animation:${object.objend - object.objstart}s step-start ${object.objstart}s spin reverse`
-		  // for (let i=0;i<keylayers.length-1;i++) {
-			  // keylayers[i+1].appendChild(keylayers[i])
-		  // }
-		  // keylayers.at(-1).appendChild(obj) //why must i use .at instead of [] 
-		  // proj.appendChild(keylayers.at(-1))
-		  proj.appendChild(obj)
-	  }
-  }
+function createPreview() { //create projector preview
+	saveChanges()
+	proj = document.getElementsByClassName("projector")[0]
+	proj.innerHTML = "" //reset
+	for (scene of movie.scenes) {
+		for (object of scene.objects) {
+			//keylayers = []
+			//keysort = object.keyframes
+			//keysort.sort(sortKeys)
+			obj = document.createElement("div")
+			obj.innerHTML = object.html
+			obj.style = `position:absolute;left:${object.objx}px;top:${object.objy}px;transform:rotatex(90deg);animation:${object.objend - object.objstart}s step-start ${object.objstart}s spin reverse` //animation makes object disappear while not in the spawn time
+			// for (let i=0;i<keylayers.length-1;i++) {
+				// keylayers[i+1].appendChild(keylayers[i]) //layer keyframes on object
+			// }
+			// keylayers.at(-1).appendChild(obj) //why must i use .at instead of [] 
+			// proj.appendChild(keylayers.at(-1))
+			proj.appendChild(obj)
+		}
+	}
 }
